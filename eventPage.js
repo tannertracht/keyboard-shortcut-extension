bda7da943b6bf96407f59f63ebc05cd9788172f8
Tanner Tracht <tannerman84@yahoo.com>
2017-10-12 00:27:04 -0600

    Added duplicate functionality

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,15 @@
 chrome.commands.onCommand.addListener(function(command) {
 	// command returns the name of the command that got fired
 	// all actions relating to shortcuts can be inside this function
-    alert("fired");
+    if (command == "boom") {
+		// Query info guarentees we grab current tab
+		var queryInfo = {
+			active: true,
+			currentWindow: true
+		};
+		chrome.tabs.query(queryInfo, (tabs) => {
+			var tab = tabs[0];
+			chrome.tabs.duplicate(tab.id);
+		});
+	};
 });
